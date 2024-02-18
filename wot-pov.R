d81481ca5815b8882b9bb031c9d28e2920108a33
Diarmuid Lloyd <diarmuid.lloyd@gmail.com>
2024-02-18 10:17:07 +0000

    Add titles

--- wot-pov.R
+++ wot-pov.R
@@ -15,3 +15,22 @@ glimpse(data)
 to_select <- seq(1,29,  by = 2)
 
 chapter_data <- data[to_select]
+
+
+book_titles <- list(
+    "New Spring",
+    "The Eye of the World",
+    "The Great Hunt",
+    "The Dragon Reborn",
+    "The Shadow Rising",
+    "The Fires of Heaven",
+    "Lord of Chaos",
+    "A Crown of Swords",
+    "The Path of Daggers",
+    "Winter's Heart",
+    "Crossroads of Twilight",
+    "Knife of Dreams",
+    "The Gathering Storm",
+    "Towers of Midnight",
+    "A Memory of Light"
+)
